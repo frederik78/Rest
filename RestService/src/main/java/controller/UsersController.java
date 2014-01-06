f62ba320024f822eeade1e2deb061ac6d26381e8
frederic.minatchy <frederik78@yahoo.fr>
2014-01-06 22:40:48 +0100

    Ajout de la possibilité de créer un User

--- RestService/src/main/java/controller/UsersController.java
+++ RestService/src/main/java/controller/UsersController.java
@@ -3,10 +3,7 @@ package controller;
 
 import beans.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import services.UsersServices;
 
 import java.text.ParseException;
@@ -17,17 +14,21 @@ import java.util.List;
 @Controller
 public class UsersController {
 
-    @RequestMapping("/user/{id}" )
+    @RequestMapping(value = "/users/user/{id}", method = RequestMethod.GET )
     public @ResponseBody User getUser(@PathVariable(value = "id") long id) throws Exception {
         return UsersServices.getService().getUser(id);
 
     }
 
-    @RequestMapping("/listOfUsers")
+    @RequestMapping(value = "/users", method = RequestMethod.GET)
     public @ResponseBody
     Collection<User> getListOfUsers() throws Exception {
         return UsersServices.getService().getListOfUsers();
     }
 
+    @RequestMapping(value = "/users/new", method = RequestMethod.POST)
+    public @ResponseBody User createUser(@RequestBody User user) throws Exception {
+        return UsersServices.getService().createUser(user);
+    }
 
 }
