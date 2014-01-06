738e8e3f99f5c1a5787b36d8874dd7a2d4c1b41e
frederic.minatchy <frederik78@yahoo.fr>
2014-01-06 22:39:34 +0100

    Ajout d'un AtomicLong pour gérer les id des nouveaux Users en cache

--- RestService/src/main/java/services/UsersServices.java
+++ RestService/src/main/java/services/UsersServices.java
@@ -5,11 +5,19 @@ import beans.User;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Created by Frederic on 28/12/13.
  */
 public class UsersServices {
+
+    /**
+     * Gestion des id
+     */
+    private static AtomicLong id = new AtomicLong(1);
+
 
     private static UsersServices usersServices  = new UsersServices();
 
@@ -34,6 +42,13 @@ public class UsersServices {
         return new HashSet<User>(result.values());
     }
 
+    public User createUser(User user)
+    {
+        user.setId(id.getAndIncrement());
+        result.put(user.getId(), user);
+        return user;
+    }
+
 
     private static void generateUsers() throws ParseException {
         final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
@@ -44,7 +59,7 @@ public class UsersServices {
         user1.setBirthday(simpleDateFormat.parse("11/05/1904"));
         user1.setPhone("01 23 45 67 89");
         user1.setEmail("salvador.dali@gmail.com");
-        user1.setId(4L);
+        user1.setId(id.getAndIncrement());
         result.put(user1.getId(), user1);
 
         final User user2 = new User();
@@ -53,7 +68,7 @@ public class UsersServices {
         user2.setBirthday(simpleDateFormat.parse("06/05/1856"));
         user2.setPhone("01 23 45 67 89");
         user2.setEmail("sigmund.freud@gmail.com");
-        user2.setId(5L);
+        user2.setId(id.getAndIncrement());
         result.put(user2.getId(), user2);
 
         final User user3 = new User();
@@ -62,7 +77,7 @@ public class UsersServices {
         user3.setBirthday(simpleDateFormat.parse("07/11/1867"));
         user3.setPhone("01 23 45 67 89");
         user3.setEmail("marie.curie@gmail.com");
-        user3.setId(2L);
+        user3.setId(id.getAndIncrement());
         result.put(user3.getId(), user3);
 
         final User user4 = new User();
@@ -71,7 +86,7 @@ public class UsersServices {
         user4.setBirthday(simpleDateFormat.parse("04/01/1643"));
         user4.setPhone("01 23 45 67 89");
         user4.setEmail("isaac.newton@gmail.com");
-        user4.setId(1L);
+        user4.setId(id.getAndIncrement());
         result.put(user4.getId(), user4);
     }
 
