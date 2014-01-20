b7f2887d078eb13cb4f0fef8aeb093759c11a9d0
Frédéric Minatchy <frederic.minatchy@gmail.com>
2014-01-20 16:02:03 +0100

    Création de la méthode :
    private static Map<String, Object> getProperties() afin d'analyser les propriétés

--- restservice/src/main/java/fr/fred/Application.java
+++ restservice/src/main/java/fr/fred/Application.java
@@ -1,13 +1,16 @@
 package fr.fred;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 @EnableWebMvc
@@ -19,9 +22,41 @@ public class Application {
     {
 
         final SpringApplication app = new SpringApplication(Application.class);
-        final Map<String, Object> parameters = new HashMap<String, Object>();
-        parameters.put("server.port", 971);
+       final Map<String, Object> parameters = getProperties();
         app.setDefaultProperties(parameters);
         app.run();
     }
+
+   private static Map<String, Object> getProperties()
+   {
+      final Properties prop = new Properties();
+      InputStream input = null;
+      final Map<String, Object> parameters = new HashMap<String, Object>();
+
+      try {
+
+         input = Application.class.getResourceAsStream("application.properties");
+
+         // load a properties file
+         prop.load(input);
+
+         // get the property value and print it out
+         final int serverPort = StringUtils.isNumeric(prop.getProperty("server.port")) ? Integer.valueOf(prop.getProperty("server.port")) : 8181;
+         final String serverHost = StringUtils.isNotEmpty(prop.getProperty("server.address")) ? prop.getProperty("server.address") : "localhost";
+         parameters.put("server.port", serverPort);
+         parameters.put("server.address", serverHost);
+
+      } catch (IOException ex) {
+         ex.printStackTrace();
+      } finally {
+         if (input != null) {
+            try {
+               input.close();
+            } catch (IOException e) {
+               e.printStackTrace();
+            }
+         }
+      }
+      return parameters;
+   }
 }
