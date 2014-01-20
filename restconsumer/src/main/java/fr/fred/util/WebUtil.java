a021a03d433c1c6f6e431d849385acf5e4bfc433
Frédéric Minatchy <frederic.minatchy@gmail.com>
2014-01-20 16:03:32 +0100

    Ajout de la méthode
     private static String getServiceLocation() qui permet de localiser le serveur distant grace aux proprités

--- restconsumer/src/main/java/fr/fred/util/WebUtil.java
+++ restconsumer/src/main/java/fr/fred/util/WebUtil.java
@@ -1,7 +1,11 @@
 package fr.fred.util;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import org.apache.commons.lang.StringUtils;
 
 /**
  * Created by Frederic on 04/01/14.
@@ -11,7 +15,7 @@ public class WebUtil {
     /**
      * Ressource distante
      */
-    public static final String SERVICE_LOCATION = "http://localhost:971";
+   public static final String SERVICE_LOCATION = getServiceLocation();
 
     /**
      * Analyse la requete REST
@@ -22,4 +26,35 @@ public class WebUtil {
     public static AnalyzeUriResource analyzeUriResource(HttpServletRequest req) throws ServletException {
         return new AnalyzeUriResource(req);
     }
+
+   private static String getServiceLocation()
+   {
+      final Properties prop = new Properties();
+      InputStream input = null;
+      String serviceLocation = "";
+      try {
+
+         input = WebUtil.class.getResourceAsStream("/application.properties");
+
+         // load a properties file
+         prop.load(input);
+
+         // get the property value and print it out
+         final int serverPort = StringUtils.isNumeric(prop.getProperty("remote.server.port")) ? Integer.valueOf(prop.getProperty("remote.server.port")) : 8181;
+         final String serverHost = StringUtils.isNotEmpty(prop.getProperty("remote.server.address")) ? prop.getProperty("remote.server.address") : "localhost";
+         serviceLocation = "http://"+serverHost+":"+serverPort;
+
+      } catch (IOException ex) {
+         ex.printStackTrace();
+      } finally {
+         if (input != null) {
+            try {
+               input.close();
+            } catch (IOException e) {
+               e.printStackTrace();
+            }
+         }
+      }
+      return serviceLocation;
+   }
 }
